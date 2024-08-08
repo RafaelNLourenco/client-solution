a74eba93a246b889f36152626dd789b179f4e6af
Rafa Lourenço <52457167+Rafasputnick@users.noreply.github.com>
2024-08-08 16:55:49 -0300

    feat: spring project and docker compose service

---
+++ back/src/test/java/com/rafael/lourenco/solucao_clientes/SolucaoClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.lourenco.solucao_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolucaoClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
